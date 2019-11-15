7b87f81b7d196307fbbae905cfeb1f480f0409f6
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2019-11-15 14:04:24 +0100

    Initial commit

---
+++ src/Pruebas.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Hello world!")
+
+    println(sum(5,20))
+    println("sum of $5 and $20 is ${5 + 20}")
+
+
+
+
+}
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+fun printSum(a: Int, b: Int): Unit {
+
+}
